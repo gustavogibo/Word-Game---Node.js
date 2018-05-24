02ea7ea1d4141bcb53a8fbee34ff1f8cff4c5a22
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-05-23 21:26:05 -0700

    finishing letter.js

---
+++ letter.js
@@ -0,0 +1,30 @@
+function Letter(underlying) { 
+    this.underlying = underlying;
+    this.guessed = false;
+    this.defineCharacter = function() {
+        if(this.guessed) {
+
+            return this.underlying;
+
+        } else {
+
+            return "_";
+
+        }
+    };
+    this.checkGuessed = function (guess) {
+
+        if(guess == this.underlying) {
+
+            this.guessed = true;
+
+        }  else {
+
+            this.guessed = false;
+
+        }
+        
+    }
+
+
+};
